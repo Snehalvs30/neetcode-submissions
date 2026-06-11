4b70397989b0b766a76abc5c6dbdede115d2da59
Snehalvs30 <shinde.sne@northeastern.edu>
2026-06-11 12:26:36 -0400

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        uniq = list(set(nums))
+        freq = []
+
+        for num in uniq:
+            count = nums.count(num)
+            freq.append((count, num))
+        freq.sort(reverse=True)
+        result = []
+        for i in range(k):
+            result.append(freq[i][1])
+        return result
+
+        # uniq = list(set(nums))
+        # freq = []
+
+        # for num in uniq:
+        #     count = nums.count(num)
+        #     freq.append((count, num))
+            
+        # freq.sort()
+
+        # result=[]
+        # for i in range(k):
+        #     result.append(freq[-k+i][1])
+        # return result
+
+
+
+        
